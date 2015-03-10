fe3c2d3f4682ba6c201315477d3104f0d16f4ec4
tkmtwo <tkmtwo@server.fake>
2015-03-10 11:07:00 -0500

    Added jointers

---
+++ core/src/main/java/com/google/common/base/TkmTwoJointers.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * Copyright 2014 Tom Mahaffey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.google.common.base;
+
+import com.google.common.base.Joiner.MapJoiner;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter.MapSplitter;
+import com.google.common.base.Splitter;
+
+
+/**
+ *
+ *
+ *
+ */
+public final class TkmTwoJointers {
+
+  public static final MapJoiner MAP_JOINER =
+    Joiner
+    .on('&')
+    .withKeyValueSeparator("=");
+
+  public static final MapSplitter MAP_SPLITTER =
+    Splitter
+    .on('&')
+    .trimResults()
+    .omitEmptyStrings()
+    .withKeyValueSeparator('=');
+  
+  
+  
+  public static final Joiner COMMA_JOINER =
+    Joiner
+    .on(',')
+    .skipNulls();
+  
+  public static final Splitter COMMA_SPLITTER =
+    Splitter
+    .on(',')
+    .omitEmptyStrings()
+    .trimResults();
+
+}
---
+++ core/src/test/java/com/google/common/base/TkmTwoJointersTest.java
@@ -0,0 +1,63 @@
+package com.google.common.base;
+
+
+import static com.google.common.base.TkmTwoStrings.isBlank;
+import static com.google.common.base.TkmTwoStrings.blankToEmpty;
+import static com.google.common.base.TkmTwoStrings.blankToNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+
+public class TkmTwoJointersTest {
+
+  
+  @Test
+  public void testComma() {
+    String s = "zero,one,two";
+    
+    List<String> l = TkmTwoJointers.COMMA_SPLITTER.splitToList(s);
+    assertEquals(3, l.size());
+    assertEquals("zero", l.get(0));
+    assertEquals("one", l.get(1));
+    assertEquals("two", l.get(2));
+    assertEquals(s, TkmTwoJointers.COMMA_JOINER.join(l));
+  }
+  
+  @Test
+  public void testCommaSpaces() {
+    String s = ", zero ,,,,one , , ,,two, , ,";
+    
+    List<String> l = TkmTwoJointers.COMMA_SPLITTER.splitToList(s);
+    assertEquals(3, l.size());
+    assertEquals("zero", l.get(0));
+    assertEquals("one", l.get(1));
+    assertEquals("two", l.get(2));
+    assertEquals("zero,one,two", TkmTwoJointers.COMMA_JOINER.join(l));
+  }
+  
+  @Test
+  public void testMap() {
+    String s = "numberZero=zero&numberOne=one&numberTwo=two";
+    
+    Map<String,String> m = TkmTwoJointers.MAP_SPLITTER.split(s);
+    assertEquals(3, m.size());
+    assertEquals("zero", m.get("numberZero"));
+    assertEquals("one", m.get("numberOne"));
+    assertEquals("two", m.get("numberTwo"));
+    
+    String js = TkmTwoJointers.MAP_JOINER.join(m);
+    assertEquals(s.length(), js.length());
+    assertTrue(js.contains("numberZero=zero"));
+    assertTrue(js.contains("numberOne=one"));
+    assertTrue(js.contains("numberTwo=two"));
+    
+  }
+    
+  
+}
